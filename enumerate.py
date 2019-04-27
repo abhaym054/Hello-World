ad110fdfd02e18f103a522a39b180d0191fa6562
abhaym054 <50039529+abhaym054@users.noreply.github.com>
2019-04-27 15:14:39 +1000

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 20:21:18 2018
+
+@author: abhay
+"""
+iterable = ["dog", "at", "cat", " ", 4, 5, 6]
+for i, n in enumerate(iterable):
+    print (i)
+    print (n)
